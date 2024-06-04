22443efefc0b0581fd3149762f497bbb6e42c179
DreamKillDemon <kietkled@gmail.com>
2024-06-04 11:58:31 +0700

    Product Table Delete, MD5 Security and something fix...

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -16,14 +16,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/View/product.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/View/home.fxml"));
         Scene scene;
         scene = new Scene(fxmlLoader.load(), 1200, 800);
         primaryStage.setScene(scene);
         // set title
         primaryStage.setTitle("DK Convenience Store");
         // set farvicon
-        Image icon = new Image(getClass().getResource("/ImageSource/farvicon.png").toExternalForm());
+        Image icon = new Image(getClass().getResource("/ImageSource/DK.png").toExternalForm());
         primaryStage.getIcons().add(icon);
         // show
         primaryStage.show();
---
+++ src/main/java/DAO/AdminDAO.java
@@ -0,0 +1,50 @@
+package DAO;
+
+import Model.Admin;
+
+import java.security.MessageDigest;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminDAO {
+    protected String md5Converter(String password) {
+        try{
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(password.getBytes());
+            byte[] digest = md.digest();
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : digest) {
+                sb.append(String.format("%02x", b));
+            }
+
+            return sb.toString();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+
+    public void addAdminAccount(Admin admin) {
+        SQLOperation.SetDatabase("insert into AdminAccount(Account, Password) values('" + admin.getAccount() + "','" + md5Converter(admin.getPassword()) + "');", "");
+
+    }
+
+    public static Admin checkAdminAccount(String account, String password) {
+        Admin admin = null;
+        try {
+            ResultSet rs = SQLOperation.GetDatabase("select* from AdminAccount "
+                    + "where Account='"+ account +"' "
+                    + "and Password= '"+ password + "'; ");
+            while (true){
+                assert rs!=null;
+                if(!rs.next()) break;
+                admin = new Admin(account, password);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return admin;
+    }
+}
